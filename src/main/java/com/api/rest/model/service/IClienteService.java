ef1215749633ecf154ee1a0059ee97b5ac89f26a
Luis David <david12.29@hotmail.com>
2024-03-03 18:10:59 -0500

    Update, the IclienteService is done

--- src/main/java/com/api/rest/model/service/IClienteService.java
+++ src/main/java/com/api/rest/model/service/IClienteService.java
@@ -1,4 +1,17 @@
 package com.api.rest.model.service;
 
+import com.api.rest.model.entity.Cliente;
+
+import java.util.List;
+
 public interface IClienteService {
+
+    List<Cliente> listAlll();
+
+    Cliente findById(Integer id);
+
+    void delete(Cliente cliente);
+
+    boolean existsById(Integer id);
+
 }
