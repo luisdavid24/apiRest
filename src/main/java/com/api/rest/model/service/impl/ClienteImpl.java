5bbe5f4afbd21ae86f39e07f0ef51fca6439722e
Luis David <david12.29@hotmail.com>
2024-03-04 10:29:40 -0500

    Update, clienteImpl class is not done

--- src/main/java/com/api/rest/model/service/impl/ClienteImpl.java
+++ src/main/java/com/api/rest/model/service/impl/ClienteImpl.java
@@ -3,6 +3,7 @@ package com.api.rest.model.service.impl;
 import com.api.rest.model.dao.ClienteDao;
 import com.api.rest.model.entity.Cliente;
 import com.api.rest.model.service.IClienteService;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,16 +15,20 @@ public class ClienteImpl implements IClienteService {
     @Autowired
     private ClienteDao clienteDao;
 
+    @Transactional
     @Override
     public Cliente save(Cliente cliente) {
         return clienteDao.save(cliente);
     }
 
+
+    @Transactional(readOnly = true)
     @Override
     public Cliente findById(Integer id) {
         return clienteDao.findById(id).orElse(null);
     }
 
+    @Transactional
     @Override
     public void delete(Cliente cliente) {
         clienteDao.delete(cliente);
