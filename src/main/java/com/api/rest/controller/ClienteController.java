3a68af6728a79e2493e0ba7191474f9e34a7e79c
Luis David <david12.29@hotmail.com>
2024-03-06 08:44:48 -0500

    Update, the controller will be done

--- src/main/java/com/api/rest/controller/ClienteController.java
+++ src/main/java/com/api/rest/controller/ClienteController.java
@@ -1,4 +1,34 @@
 package com.api.rest.controller;
 
-public class ClienteController {
+
+import com.api.rest.model.entity.Cliente;
+import com.api.rest.model.service.IClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ClienteController  {
+
+    @Autowired
+    private IClienteService iClienteService;
+
+    public Cliente create(Cliente cliente){
+        return iClienteService.save(cliente);
+    }
+    public Cliente update(Cliente cliente){
+        return iClienteService.save(cliente);
+    }
+
+    public void  delete(Integer id){
+         Cliente clienteDelete= iClienteService.findById(id);
+         iClienteService.save(clienteDelete);
+    }
+
+    public Cliente showById(Integer id){
+        return iClienteService.findById(id);
+    }
+
+
 }
