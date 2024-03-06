eaff164f2272d317e7b65ecc1e38575427fcb440
Luis David <david12.29@hotmail.com>
2024-03-06 08:51:16 -0500

    Update, the controller is done by me

--- src/main/java/com/api/rest/controller/ClienteController.java
+++ src/main/java/com/api/rest/controller/ClienteController.java
@@ -4,10 +4,7 @@ package com.api.rest.controller;
 import com.api.rest.model.entity.Cliente;
 import com.api.rest.model.service.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -21,17 +18,19 @@ public class ClienteController  {
         return iClienteService.save(cliente);
     }
 
-
-    public Cliente update(Cliente cliente){
+    @PutMapping("cliente")
+    public Cliente update(@RequestBody  Cliente cliente){
         return iClienteService.save(cliente);
     }
 
-    public void  delete(Integer id){
+    @DeleteMapping("cliente/{id}")
+    public void  delete(@PathVariable  Integer id){
          Cliente clienteDelete= iClienteService.findById(id);
          iClienteService.save(clienteDelete);
     }
 
-    public Cliente showById(Integer id){
+    @GetMapping("cliente/{id}")
+    public Cliente showById(@PathVariable Integer id){
         return iClienteService.findById(id);
     }
 
