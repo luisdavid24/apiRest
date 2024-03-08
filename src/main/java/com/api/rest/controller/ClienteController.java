5720637c436223bb55a447b12f3e5b20b132cfc0
Luis David <david12.29@hotmail.com>
2024-03-08 14:02:50 -0500

    Update, the clienteController has some changed about httpStatus

--- src/main/java/com/api/rest/controller/ClienteController.java
+++ src/main/java/com/api/rest/controller/ClienteController.java
@@ -4,6 +4,7 @@ package com.api.rest.controller;
 import com.api.rest.model.entity.Cliente;
 import com.api.rest.model.service.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,22 +15,26 @@ public class ClienteController  {
     private IClienteService iClienteService;
 
     @PostMapping("cliente")
+    @ResponseStatus(HttpStatus.CREATED)
     public Cliente create(@RequestBody Cliente cliente){
         return iClienteService.save(cliente);
     }
 
     @PutMapping("cliente")
+    @ResponseStatus(HttpStatus.CREATED)
     public Cliente update(@RequestBody  Cliente cliente){
         return iClienteService.save(cliente);
     }
 
     @DeleteMapping("cliente/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void  delete(@PathVariable  Integer id){
          Cliente clienteDelete= iClienteService.findById(id);
          iClienteService.save(clienteDelete);
     }
 
     @GetMapping("cliente/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Cliente showById(@PathVariable Integer id){
         return iClienteService.findById(id);
     }
