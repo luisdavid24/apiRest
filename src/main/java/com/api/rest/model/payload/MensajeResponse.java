84e5fbde7aa840c33fab0b81218a2a7c2f15a992
Luis David <david12.29@hotmail.com>
2024-03-13 13:38:13 -0500

    Update, the mensajeResponse has some changed

--- src/main/java/com/api/rest/model/payload/MensajeResponse.java
+++ src/main/java/com/api/rest/model/payload/MensajeResponse.java
@@ -1,4 +1,15 @@
 package com.api.rest.model.payload;
 
-public class MensajeResponse {
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@Data
+@ToString
+@Builder
+public class MensajeResponse implements Serializable {
+    private String mensaje;
+    private Object object;
 }
